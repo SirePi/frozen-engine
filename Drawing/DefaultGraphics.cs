ce6fcda76541ca37720974768c04289d65b64495
SirePi <yamailbox@gmail.com>
2022-10-04 08:34:03 +0200

    Colors and Material fixes

--- Drawing/DefaultGraphics.cs
+++ Drawing/DefaultGraphics.cs
@@ -8,7 +8,12 @@ namespace Frozen.Drawing
 	public static class DefaultGraphics
 	{
 		public static AlphaTestEffect AlphaTestTexture { get; private set; } = new AlphaTestEffect(Engine.Game.GraphicsDevice) { VertexColorEnabled = true };
-		public static BasicEffect DefaultTexture { get; private set; } = new BasicEffect(Engine.Game.GraphicsDevice) { TextureEnabled = true, VertexColorEnabled = true };
+		public static AlphaTestEffect DefaultTexture { get; private set; } = new AlphaTestEffect(Engine.Game.GraphicsDevice) { VertexColorEnabled = true };
 		public static BasicEffect FlatColor { get; private set; } = new BasicEffect(Engine.Game.GraphicsDevice) { TextureEnabled = false, VertexColorEnabled = true };
+
+		static DefaultGraphics()
+		{
+			DefaultTexture.FogEnabled = false;
+		}
 	}
 }
